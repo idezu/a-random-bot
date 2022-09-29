2942294ec7a66ba9efe6f0fb99a54b3f3f492918
idezu <mpnmph@gmail.com>
2022-09-29 18:10:37 +0200

    added comments

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-const Discord = require('discord.js');
-const config = require("../config.json");
-/* the config file look like
- 
+// read the config 
+const config = require("config.json");
+var prefix = config.prefix;
+const token = config.token
+
+//              REMARK
+/*  you have to create the config.json on your on 
+the config file look like
+
 {
-    "token" : "your token"
-    "prefix" : "your prefix"
+  "token" : "your token",
+  "prefix" : "your prefix"
 }
 
 you have to create it
 */
 const fs = require("fs");
 const { error } = require('console');
+
+// discordjs initialization and declare the client
+const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection()
-var prefix = config.prefix;
-const token = config.token
 
+//            the command handler
+
+// read the dir ./command for the command
 fs.readdir("./commands/", (err, files) =>{
   if(err) console.log(err);
-
+  
   var jsFiles = files.filter(f => f.split(".").pop() === "js");
   if(jsFiles.length <= 0){
-      console.log("No command file!")
-      return;
+    console.log("No command file!")
+    return;
   }
   jsFiles.forEach((f,i) =>{
-      var fileGet = require("./commands/" + f);
-      console.log("File command " + f + " successfully recovered !")
-      client.commands.set(fileGet.help.name, fileGet)
-    });
+    var fileGet = require("./commands/" + f);
+    console.log("File command " + f + " successfully recovered !")
+    client.commands.set(fileGet.help.name, fileGet)
+  });
 });
 
+//  login
+client.login(token);
+
+// define the activity
 client.on("ready", () => {
   console.log("connected at "+client.user.tag); 
   console.log("a random client is curently online."); const status = [
@@ -39,7 +52,7 @@ client.on("ready", () => {
       type: "WATCHING",
     },
     {
-      activity: "Based on Roboliam",
+      activity: "Based on Roboliam by Controlfreak707",
       type: "PLAYING",
     },
     {
@@ -70,6 +83,8 @@ client.on("ready", () => {
   });
 })
 
+// where the magic begin
+// when a message is send on the server
 client.on('message', (msg) => { 
   
   if(msg.channel.type === "dm") 
@@ -85,34 +100,23 @@ client.on('message', (msg) => {
 
 
   if(msg.content == `<@${client.user.id}>` ||
-     msg.content == `<@!${client.user.id}>`|| msg.content == "?" 
-                                                  )
+     msg.content == `<@!${client.user.id}>`|| msg.content == "?" )
   {
     var commands = client.commands.get("help")
-  }
-//exocommand usage
-  if (command == prefix+"usage")
+  } 
+  commands.run(client, msg, args);
+  console.log("the message has been treated")
+  if (error)
   {
-    var commands = client.commands.get(args)
-    msg.channel.send(commands.help.description(), commands.help.usage())
-  }  
-  else if (commands)
-  { 
-    commands.run(client, msg, args);
-    console.log("the message has been treated")
-    if (error)
-    {
-        console.log(console.error)
-    }
+      console.log(console.error)
   }
   
 })
 
+// when a member join a server
 client.on('guildMemberAdd', member => {
   member.createDM().then(channel => {
     return channel.send('welcome to this server ' + member.displayName + "go read the rules please!!!!")
   }).catch(console.error)
   
 })
-
-client.login(token);

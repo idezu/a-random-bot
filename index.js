630db6d1fdbe45b4487639da428fcd0a5686be57
idezu <mpnmph@gmail.com>
2022-11-22 19:52:20 +0100

    some refactoring of the code

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*
+        A MULTIPURPOSE BOT FOR DISCORD USING DISCORD.JS
+*/
 // read the config 
 const config = require("./config.json");
 var prefix = config.prefix;
@@ -5,11 +8,11 @@ const token = config.token
 
 //              REMARK
 /*  you have to create the config.json on your on 
-the config file look like
+the config file look like this:
 
 {
-  "token" : "your token",
-  "prefix" : "your prefix"
+    "token" : "your token",
+    "prefix" : "your prefix"
 }
 
 */
@@ -21,22 +24,58 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection()
 
+// status for the bot
+const status = [
+    {
+        activity: ".help | @a random client",
+        type: "WATCHING",
+    },
+    {
+        activity: "Based on Roboliam by Controlfreak707",
+        type: "PLAYING",
+    },
+    {
+        activity: "Minecraft love this game !!!!! :heart:",
+        type: "PLAYING",
+    },
+    {
+        activity: "Exterminate dragibus!!!!!!!",
+        type: "PLAYING",
+    },
+    {
+        activity: "Your cries of pain",
+        type: "LISTENING",
+    },
+    {
+        activity: "Furry YouTubers...",
+        type: "WATCHING",
+    },
+    {
+        activity: "Check Gravity rush or i'll eat your mom!!!!",
+        type: "WATCHING",
+    },
+    {
+        activity: "signalis 'cause it's a pretty good game",
+        type: "PLAYING",
+    }
+];
+
 //            the command handler
 
 // read the dir ./command for the command
 fs.readdir("./commands/", (err, files) =>{
-  if(err) console.log(err);
-  
-  var jsFiles = files.filter(f => f.split(".").pop() === "js");
-  if(jsFiles.length <= 0){
-    console.log("No command file!")
-    return;
-  }
-  jsFiles.forEach((f,i) =>{
-    var fileGet = require("./commands/" + f);
+    if(err) console.log(err);
+
+    let jsFiles = files.filter(f => f.split(".").pop() === "js");
+    if(jsFiles.length <= 0){
+        console.log("No command file!")
+        return;
+    }
+    jsFiles.forEach((f,i) =>{
+    let fileGet = require("./commands/" + f);
     console.log("File command " + f + " successfully recovered !")
     client.commands.set(fileGet.help.name, fileGet)
-  });
+    });
 });
 
 //  login
@@ -44,74 +83,42 @@ client.login(token);
 
 // define the activity
 client.on("ready", () => {
-  console.log("connected at "+client.user.tag); 
-  console.log("a random client is curently online."); const status = [
-    {
-      activity: ".help | @a random client",
-      type: "WATCHING",
-    },
-    {
-      activity: "Based on Roboliam by Controlfreak707",
-      type: "PLAYING",
-    },
-    {
-      activity: "Minecraft love this game !!!!! :heart:",
-      type: "PLAYING",
-    },
-    {
-      activity: "Exterminate dragibus!!!!!!!",
-      type: "PLAYING",
-    },
-    {
-      activity: "Your cries of pain",
-      type: "LISTENING",
-    },
-    {
-      activity: "Furry YouTubers...",
-      type: "WATCHING",
-    }
-  ];
-  let random = status[Math.floor(Math.random() * Math.floor(status.length))];
-  // for a randoms status
-  client.user.setActivity(random.activity, {
+    console.log("connected at "+client.user.tag); 
+    console.log("a random client is curently online.");
+    // status is define as constant earlier
+    let random = status[Math.floor(Math.random() * Math.floor(status.length))];
+    // for a randoms status
+    client.user.setActivity(random.activity, {
     type: random.type,
-  });
+    });
 })
 
 // where the magic begin
 // when a message is send on the server
 client.on('message', (msg) => { 
-  
-  if(msg.channel.type === "dm") 
-  {
-    return;
-  }
-  console.log("the message is in a server")
+    // cancell if it's in a dm
+    if(msg.channel.type === "dm") return;
 
-  var msgArray = msg.content.split(" ");
-  var command = msgArray[0];
-  var args = msgArray.slice(1)
-  var commands = client.commands.get(command.slice(prefix.length))
+    console.log("the message is in a server")
 
+    let msgArray = msg.content.split(" "), prefixed_command = msgArray[0];
 
-  if(msg.content == `<@${client.user.id}>` ||
-     msg.content == `<@!${client.user.id}>`|| msg.content == "?" )
-  {
-    var commands = client.commands.get("help")
-  } 
-  commands.run(client, msg, args);
-  console.log("the message has been treated")
-  if (error)
-  {
-      console.log(console.error)
-  }
-  
+    // cancel if it's not starting with the prefix
+    if(!prefixed_command.startsWith(prefix)) return;
+
+    console.log("the message start with "+ prefix)
+
+    let args = msgArray.slice(1), command = client.command.get(prefixed_command.slice(prefix.length));
+
+    command.run(client, msg, args);
+    console.log("the command has been treated")
+    if (error) console.log(console.error);
+
 })
 
 // when a member join a server
-client.on('guildMemberAdd', member => {
-  member.createDM().then(channel => {
-    return channel.send('welcome to this server ' + member.displayName + "go read the rules please!!!!")
-  }).catch(console.error)
-  
+client.on('guildMemberAdd', member => { member.createDM().then(channel => {
+        return channel.send('welcome to this server ' + member.displayName + "go read the rules please!!!!")
+    })
+    .catch(console.error)
 })
